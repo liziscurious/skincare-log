3e4aa214ce07336ce296d5742c4d1f007e00d76e
liziscurious <32523128+liziscurious@users.noreply.github.com>
2018-01-23 23:21:40 -0800

    JWT Login Working on Front End

--- public/js/app.js
+++ public/js/app.js
@@ -3,5 +3,43 @@ const app = angular.module('SkincareLog', []);
 app.controller('MainController', ['$http', function($http){
 
   this.message = 'How many steps are in your routine?';
+  this.url = 'http://localhost:3000';
+  this.user = {};
+
+  this.login = function(userPass) {
+    console.log(userPass);
+    $http({
+      method: 'POST',
+      url: this.url + '/users/login',
+      data: { user: { username: userPass.username, password: userPass.password }},
+    }).then(function(response) {
+      console.log(response);
+      this.user = response.data.user;
+      localStorage.setItem('token', JSON.stringify(response.data.token));
+    }.bind(this));
+  };
+
+  this.getUsers = function() {
+    $http({
+      url: this.url + '/users',
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('token'))
+      }
+    }).then(function(response) {
+      console.log(response);
+      if (response.data.status == 401) {
+          this.error = "Unauthorized";
+      } else {
+        this.users = response.data;
+      }
+    }.bind(this));
+  };
+
+  this.logout = function () {
+    localStorage.clear('token');
+    location.reload();
+  };
+
 
 }]);
