b241dda3a9f6d6ef024e5b3e9cb91b65cddcbed7
liziscurious <32523128+liziscurious@users.noreply.github.com>
2018-01-23 23:45:46 -0800

    Categories are displaying properly

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,11 @@ const app = angular.module('SkincareLog', []);
 app.controller('MainController', ['$http', function($http){
 
   this.message = 'How many steps are in your routine?';
-  this.url = 'http://localhost:3000';
+  this.url = 'http://localhost:3000/';
   this.user = {};
+  this.showLogInForm = false;
+  this.showCategories = false;
+  this.categories = [];
 
   this.login = function(userPass) {
     console.log(userPass);
@@ -41,5 +44,17 @@ app.controller('MainController', ['$http', function($http){
     location.reload();
   };
 
+  // Show All Categories Route
+  this.getCategories = () => {
+    $http({
+      method: 'GET',
+      url: this.url + 'categories'
+    }).then(response => {
+      this.categories = response.data;
+      console.log(this.categories);
+    }).catch(err => console.log(err));
+  };
+
+  this.getCategories();
 
 }]);
